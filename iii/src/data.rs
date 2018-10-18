54bfa18206a87cc4daa32e0fbcff82428bf1815e
Newton Ni <nwtnni@gmail.com>
2018-10-18 09:34:44 -0400

    Add overall game state and players to data

--- iii/src/data.rs
+++ iii/src/data.rs
@@ -1,3 +1,19 @@
+pub struct State {
+    pub id: usize,
+    pub width: usize,  
+    pub height: usize,
+    pub players: Vec<Player>,
+    pub halite: Vec<usize>,
+}
+
+pub struct Player {
+    pub id: usize,
+    pub yard: Shipyard, 
+    pub drops: Vec<Dropoff>,
+    pub ships: Vec<Ship>,
+    pub halite: usize,
+}
+
 pub struct Ship {
     pub owner: usize,
     pub id: usize,
