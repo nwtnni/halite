001806ac19777143d746a281acfe752471af9515
Newton Ni <nwtnni@gmail.com>
2018-10-18 09:22:20 -0400

    Add data types

---
+++ iii/src/data.rs
@@ -0,0 +1,21 @@
+pub struct Ship {
+    pub owner: usize,
+    pub id: usize,
+    pub x: usize,
+    pub y: usize,
+    pub halite: usize,
+}
+
+pub struct Shipyard {
+    pub owner: usize,
+    pub id: usize,
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Dropoff {
+    pub owner: usize,
+    pub id: usize,
+    pub x: usize,
+    pub y: usize,
+}
--- iii/src/lib.rs
+++ iii/src/lib.rs
@@ -1,2 +1,3 @@
 mod command;
+mod data;
 mod grid;
